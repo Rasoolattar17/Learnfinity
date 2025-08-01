90b8070fdc83d190a6b6a7772ed30ffd6951daab
Rasoolattar17 <your-email@example.com>
2025-08-01 18:40:08 +0530

    Add initial implementation of local test plugin
    - Created the basic structure for a new Moodle local plugin named 'Test Plugin'.
    - Implemented main plugin page (index.php) with user notifications and plugin status display.
    - Added library functions for welcome message and plugin status checks in lib.php.
    - Defined plugin settings and capabilities in settings.php and access.php respectively.
    - Included versioning information in version.php and language strings in lang/en/local_testplugin.php.
    - Provided a README.md for installation and usage instructions.

---
+++ local/testplugin/lang/en/local_testplugin.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for local_testplugin
+ *
+ * @package    local_testplugin
+ * @copyright  2025 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['description'] = 'This is a simple test plugin for testing CI/CD workflows.';
+$string['enabled'] = 'Enable test plugin';
+$string['enabled_desc'] = 'Enable or disable the test plugin functionality.';
+$string['pluginname'] = 'Test Plugin';
+$string['settings'] = 'Test Plugin Settings';
+$string['testplugin:view'] = 'View test plugin';
+$string['welcome'] = 'Welcome to Test Plugin';
